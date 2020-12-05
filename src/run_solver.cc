e7402b3901389cf24dc01ce7b291df58fd1cb1e2
shines77 <gz_shines@msn.com>
2020-12-06 05:47:04 +0800

    Fixed Sudoku::display_board(solution);
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/run_solver.cc
+++ src/run_solver.cc
@@ -321,12 +321,12 @@ void run_a_testcase(size_t index)
         sw.stop();
         double elapsed_time = sw.getElapsedMillisec();
 
-		Sudoku::display_board(board);
+		Sudoku::display_board(solution);
 
-        printf("elapsed time: %0.3f ms, init_counter: %" PRIuPTR ", recur_counter: %" PRIuPTR "\n\n"
+        printf("elapsed time: %0.3f ms, num_solutions: %" PRIuPTR ", recur_counter: %" PRIuPTR "\n\n"
                "num_guesses: %" PRIuPTR ", num_impossibles: %" PRIuPTR ", no_guess: %" PRIuPTR "\n"
                "guess %% = %0.1f %%, impossible %% = %0.1f %%, no_guess %% = %0.1f %%\n\n",
-               elapsed_time, (size_t)0, num_guesses,
+               elapsed_time, num_solutions, (size_t)0, num_guesses,
                num_guesses,
                (size_t)0,
                (size_t)0,
