767cdab32b1a5157828ceade607b77ced060612c
shines77 <gz_shines@msn.com>
2020-12-06 06:41:08 +0800

    Modify format style
    Signed-off-by: shines77 <gz_shines@msn.com>

--- src/run_solver.cc
+++ src/run_solver.cc
@@ -379,7 +379,7 @@ void run_tdoku_test(const char * filename)
 
     printf("Total puzzle count = %u\n\n", (uint32_t)puzzleCount);
     printf("Total elapsed time: %0.3f ms\n\n", total_time);
-    printf("num_guesses: %" PRIuPTR ", no_guess: %" PRIuPTR "\n"
+    printf("num_guesses: %" PRIuPTR ", no_guess: %" PRIuPTR "\n\n"
            "guess %% = %0.1f %%, no_guess %% = %0.1f %%\n\n",
            total_guesses, total_no_guess,
            (100.0 - no_guess_percent), no_guess_percent);
